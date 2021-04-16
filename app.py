7d24a821dc8d09c1939e5b6472985d242ba42e63
anshul1004 <anshul.pardhi@gmail.com>
2021-04-15 21:02:33 -0500

    Added CRUD operations on backend for menu

--- app.py
+++ app.py
@@ -1,28 +1,30 @@
-
 from flask import Flask, request, json, session, render_template
 from bson.objectid import ObjectId
 import pymongo
 
 # Create
 # Read
-# Update 
+# Update
 # Delete
 
 app = Flask(__name__)
 
-myclient = pymongo.MongoClient("mongodb+srv://mamamiaadmin:mamamiapassword@italianrestaurant.okus2.mongodb.net/mamamia?retryWrites=true&w=majority")
+myclient = pymongo.MongoClient(
+    "mongodb+srv://mamamiaadmin:mamamiapassword@italianrestaurant.okus2.mongodb.net/mamamia?retryWrites=true&w=majority")
 mydb = myclient["mamamia"]
 users = mydb["users"]
+menu = mydb["menu"]
+
 
 @app.route("/")
 def index():
-	if 'username' in session:
-		return 'You are logged in as '+ session['username']
-	return render_template('signin.html');
+    if 'username' in session:
+        return 'You are logged in as ' + session['username']
+    return render_template('signin.html')
 
-#Read - List all Users
+# Read - List all Users
 @app.route("/users")
-def main(): 
+def main():
     response = []
     for record in users.find():
         record['_id'] = str(record['_id'])
@@ -30,14 +32,12 @@ def main():
     return json.dumps(response)
 
 
-
 # Read - list and individual User
 @app.route('/users/<id>')
 def showVideo(id):
-	# return json.dumps({'video id': videoid })
-	record = users.find_one({'_id' : ObjectId(id)})
-	record['_id'] = str(record['_id'])
-	return json.dumps(record)
+    record = users.find_one({'_id': ObjectId(id)})
+    record['_id'] = str(record['_id'])
+    return json.dumps(record)
 
 
 # Create - add a new video
@@ -51,37 +51,77 @@ def showVideo(id):
 # Create - add a new user
 @app.route('/users', methods=['POST'])
 def newUser():
-	new = request.get_json()
-	_id = users.insert_one(new)
-	return json.dumps({'message': 'User created successfully !'})
+    new = request.get_json()
+    _id = users.insert_one(new)
+    return json.dumps({'message': 'User created successfully !'})
 
 # Update - update an existing user
-@app.route('/videos/<id>', methods=['PUT'])
+@app.route('/users/<id>', methods=['PUT'])
 def updateVideo(id):
 
-	# title = request.args.get('title')
-	# desc  = request.args.get('desc')
-	new = request.get_json()
-	query = { '_id':  ObjectId(id) }
-	# newvalues = { "$set": { 'title': title, "description": desc  } }
-	newvalues = {"$set": new }
-	users.update_one(query, newvalues)
+    # title = request.args.get('title')
+    # desc  = request.args.get('desc')
+    new = request.get_json()
+    query = {'_id':  ObjectId(id)}
+    # newvalues = { "$set": { 'title': title, "description": desc  } }
+    newvalues = {"$set": new}
+    users.update_one(query, newvalues)
 
-	return json.dumps({'message': 'User updated successfully !'})
+    return json.dumps({'message': 'User updated successfully !'})
 
-# Delete - delete a video
-@app.route('/videos/<id>', methods=['DELETE'])
+# Delete - delete a user
+@app.route('/users/<id>', methods=['DELETE'])
 def deleteVideo(id):
 
-	query = { '_id':  ObjectId(id) }
-	users.delete_one(query)
+    query = {'_id':  ObjectId(id)}
+    users.delete_one(query)
 
-	return json.dumps({'message': 'User deleted successfully !'})
+    return json.dumps({'message': 'User deleted successfully !'})
 
 
+# Read - List all Menu items
+@app.route("/menu")
+def getMenu():
+    response = []
+    for record in menu.find():
+        record['_id'] = str(record['_id'])
+        response.append(record)
+    return json.dumps(response)
+
+
+# Read - Show individual menu item
+@app.route('/menu/<id>')
+def getMenuItem(id):
+    record = menu.find_one({'_id': ObjectId(id)})
+    record['_id'] = str(record['_id'])
+    return json.dumps(record)
+
+
+# Create - add a new menu item
+@app.route('/menu', methods=['POST'])
+def newMenuItem():
+    new_menu_item = request.get_json()
+    _id = menu.insert_one(new_menu_item)
+    return json.dumps({'message': 'Menu item created successfully !'})
+
+
+# Update - update an existing menu item
+@app.route('/menu/<id>', methods=['PUT'])
+def updateMenuItem(id):
+    menu_item = request.get_json()
+    query = {'_id':  ObjectId(id)}
+    updated_menu_item = {"$set": menu_item}
+    menu.update_one(query, updated_menu_item)
+    return json.dumps({'message': 'Menu item updated successfully !'})
+
+
+# Delete - delete a menu item
+@app.route('/menu/<id>', methods=['DELETE'])
+def deleteMenuItem(id):
+    query = {'_id':  ObjectId(id)}
+    menu.delete_one(query)
+    return json.dumps({'message': 'Menu item deleted successfully !'})
 
 
 if __name__ == "__main__":
-    app.run()   
-
-
+    app.run()
