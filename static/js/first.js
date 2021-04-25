e444e2f650db59de7010e14d3737533ab70b9f51
kushagradarkdk <darkushagrakdk@outlook.com>
2021-04-24 20:06:23 -0500

    User Verification with SignUp and SignIn

--- static/js/first.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-
-    $("#basic").click(function () {
-        if ($("#basic").val() === "signup");
-        window.location.replace("signup.html");
-    });
-
-});
-
---
+++ static/mamamiaJs/signin.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+
+    $('#btnSignIn').click(function() {
+        let email = document.getElementById("inputUsername");
+        let password = document.getElementById("inputPassword");
+        console.log(email)
+        console.log(password)
+        $.ajax({
+            url: '/signIn',
+            data: $('form').serialize(),
+            type: 'POST',
+            success: function(response) {
+                console.log(response)
+                    var res = JSON.parse(response);
+                    if(res.message == "Username doesnt exist")
+                        {
+                            $("#helper-text-signin-email").removeClass("helper-text-invisible")
+                            .addClass("helper-text-visible")
+
+                            email.classList.add("input-border-color");
+                        }
+                    else {
+                        $("#helper-text-signin-email").removeClass("helper-text-visible")
+                        .addClass("helper-text-invisible");
+            
+                        email.classList.remove("input-border-color")
+                        }
+                    if(res.message == "Password incorrect!")
+                        {
+                            $("#helper-text-signin-password").removeClass("helper-text-invisible")
+                            .addClass("helper-text-visible")
+
+                            password.classList.add("input-border-color");
+                        }
+                    else {
+                            $("#helper-text-signin-password").removeClass("helper-text-visible")
+                            .addClass("helper-text-invisible");
+
+                            password.classList.remove("input-border-color")
+                        }
+                    if(res.message == "user verified!") 
+                        window.location.href = "/userLandingPage";
+            },
+            error: function(error) {
+                console.log(error);
+            }
+      });
+  });
+    
+});
+
